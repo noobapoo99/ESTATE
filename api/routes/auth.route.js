fc5d7d322e31f4ca31734694d65ebe7a29567615
noobapoo99 <apoorvtripathi99@gmail.com>
2024-07-12 16:58:32 +0530

    ini commit

---
+++ api/routes/auth.route.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { login, logout, register } from "../controllers/auth.controller.js";
+const router = express.Router();
+
+router.post("/register", register);
+router.get("/login", login);
+router.get("/logout", logout);
+
+export default router;
